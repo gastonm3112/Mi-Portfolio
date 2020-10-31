7106cc83b58bde1ad131b1375d26e8e3762a1ea6
Gaston Martinez <gastonm3112@gmail.com>
2020-10-31 10:23:48 -0300

    Cree formulario con Validación

---
+++ main.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("#contact_form");
+const name = document.querySelector("#name");
+const nameHelp = document.querySelector("#nameHelp");
+const email = document.querySelector("#email");
+const emailHelp = document.querySelector("#emailHelp");
+const sendMessage = document.querySelector("#sendMessage");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    if(name.value === "" || email === ""){
+        if(name.value === ""){
+            name.style.border = "1px solid #ec0101";
+            nameHelp.innerHTML = "¡Por favor, completa este campo!";
+        } if(email.value === ""){
+            email.style.border = "1px solid #ec0101";
+            emailHelp.innerHTML = "¡Por favor, completa este campo!";
+        }
+    } else {
+        form.submit();
+        sendMessage.innerHTML = "Tu mensaje ha sido envíado";
+    }
+})
