e55493d4f0d607ffa4206bbd74427c315d86ddba
Gaston Martinez <gastonm3112@gmail.com>
2020-11-02 18:22:46 -0300

    Removi mensaje de éxito del Form

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const name = document.querySelector("#name");
 const nameHelp = document.querySelector("#nameHelp");
 const email = document.querySelector("#email");
 const emailHelp = document.querySelector("#emailHelp");
-const sendMessage = document.querySelector("#sendMessage");
+
 
 form.addEventListener("submit", e => {
     e.preventDefault();
@@ -16,8 +16,7 @@ form.addEventListener("submit", e => {
             email.style.border = "1px solid #ec0101";
             emailHelp.innerHTML = "¡Por favor, completa este campo!";
         }
-    } else {
-        sendMessage.innerHTML = "Tu mensaje ha sido envíado";
-        form.submit();
+    } else {   
+       form.submit();
     }
 })
